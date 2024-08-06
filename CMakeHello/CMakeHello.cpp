ded528b54b7a6eeb256bb720015c770c7a473cb7
Tim Gilevich <ouncetim@gmail.com>
2024-08-06 23:06:16 +0300

    Init

---
+++ CMakeHello/CMakeHello.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeHello.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeHello.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
